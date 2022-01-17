b7cadc7b8e833c7f2e9d5456b2e43190908d32e6
Pocholo <plpantoja49@gmail.com>
2022-01-17 11:45:28 +0800

    feat: endpoint for training with custom dataset

--- app/main.py
+++ app/main.py
@@ -21,6 +21,69 @@ app.config['TARGET_ORIGIN'] = '*'
 def index():
     return "<p>Server is up and running</p>"
 
+@app.route('/lof_train', methods=['POST'])
+@cross_origin(origin=app.config['TARGET_ORIGIN'], headers=['Content-Type'])
+def lof_train():
+    if request.method == 'POST':
+        # fetch received data
+        request_data = request.get_json()
+
+        # get the data and the columns
+        data = request_data['normal'] + request_data['abnormal']
+        columns = request_data['columns']
+
+        # transform received data to a DataFrame
+        combined = pd.DataFrame(data, columns=columns)
+
+        # local outlier factor
+        lof = LocalOutlierFactor()
+
+        x = combined.drop(['Target'], axis=1)
+        y = combined['Target']
+
+        # initialize variables to return
+        predictions = []
+
+        for i in range(len(x)):
+            inputs = x.values[i].reshape(-1, 1)
+            prediction = lof.fit_predict(inputs)
+            if -1 not in prediction:
+                predictions.append('Normal')
+            else:
+                predictions.append('Abnormal')
+        
+        n = 0
+        a = 0
+        na = 0
+        an = 0
+        score = 0
+
+        for i, j in zip(y, predictions):
+            # get accuracy
+            if i==j:
+                score+=1
+            
+            # get confusion matrix
+            if i==j and i=='Normal':
+                n+=1
+            elif i==j and i=='Abnormal':
+                a+=1
+            elif i!=j and i=='Normal':
+                na+=1
+            elif i!=j and i=='Abnormal':
+                an+=1
+        
+        accuracy = str((score/len(x))*100)
+
+        print('Accuracy: ' + accuracy)
+        print('Classified Normal as Normal: '+str(n))
+        print('Classified Abnormal as Abnormal: '+str(a))
+        print('Classified Normal as Abnormal: '+str(na))
+        print('Classified Abnormal as Normal: '+str(an))
+
+        return { "accuracy": accuracy, "true_normal": n, "true_abnormal": a, "false_normal": an, "false_abnormal": na }
+
+
 @app.route('/lof_test', methods=['POST'])
 @cross_origin(origin=app.config['TARGET_ORIGIN'], headers=['Content-Type'])
 def lof_test():
